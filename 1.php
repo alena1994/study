055589741ced6b2684a031a78f9f65e4e4c6047c
Романенко Алена <ra@crtweb.ru>
2017-01-27 13:13:11 +0500

    final fix

--- 1.php
+++ 1.php
@@ -2,12 +2,12 @@
 
 $i = 0;
 for ($i == 1; $i < 5; $i++) {
-    echo $i . "<br>";
+    echo $i . '<br>';
 }
 
-$mas = [1, 2, 3,];
+$mas = [1, 2, 3];
 foreach ($mas as $key => $val) {
-    echo $key . "-" . $val . "<br";
+    echo $key . '-' . $val . '<br>';
 }
 
 $i = 0;
@@ -23,56 +23,55 @@ do {
 
 $i = 0;
 if ($i < 4) {
-echo $i;
+    echo $i;
 } elseif ($i > 5) {
     echo $i;
 } else {
-    echo "no";
+    echo 'no';
 }
 
 switch ($i) {
-    case 0: 
+    case 0:
         echo $i;
         break;
-	case 1:
-	    echo $i;
-		break;
-	default: 
-	    echo "no";
+    case 1:
+        echo $i;
+        break;
+    default: 
+	    echo 'no';
 }
 
-function funSunThe ()
+public function funSunThe()
 {
     $i = 1 + 5;
 }
 
 class ClassName 
-{
-public $a;
-private $b;
-public $l = 1;
-public $k = 5;
+{	
+    public $a;
+    private $b;
+    public $l = 1;
+    public $k = 5;
 
+    public function funTheThe($c, $s) 
+    {
+        $i = mod($c, $s);
+    }
 
-function funTheThe ($c, $s) 
-{
-    $i = mod($c, $s);
-}
-
-function funSub ($d)
-{
-	$mul1 = 0;
-	$mul2 = 0;
-    for ($i = 0; $i <= 2; $i++) {
-	    $mul1 = $mul1 + $d[$i];
-	}
-	for ($i = 3; $i <= 6; $i++) {
-	    $mul2 = $mul2 + $d[$i];  
-	}
-    if ($mul1 == $mul2) {
-	echo " lucky ticket";
-	}		
-}
+    public function funSub($d)
+    {
+        $mul1 = 0;
+        $mul2 = 0;
+        for ($i = 0; $i <= 2; $i++) {
+            $mul1 = $mul1 + $d[$i];
+        }
+        for ($i = 3; $i <= 6; $i++) {
+            $mul2 = $mul2 + $d[$i];  
+        }
+        if ($mul1 == $mul2) {
+        echo 'lucky ticket';
+	    }		
+    }
 }
 
 $ob = new ClassName();
